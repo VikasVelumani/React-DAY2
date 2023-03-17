610f8bc026a2f5e662d300d620d31a875e302db9
VikasVelumani <108916684+VikasVelumani@users.noreply.github.com>
2023-03-17 16:31:16 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import './App.css';
+import World from './Component/World';
+
+function App() {
+  return (
+    <div className="App">
+      Hello world 
+      <World/>
+    </div>
+  );
+}
+
+export default App;
---
+++ World.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function World() {
+  return (
+    <div>This is World Functional Component</div>
+  )
+}
+
+export default World
